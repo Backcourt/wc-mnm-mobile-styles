89ba35c2887b546dd5f48d78a62e718ac4dea210
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2024-05-10 12:00:40 -0400

    convert indentation from spaces to tabs

--- src/components/add-to-cart-button.js
+++ src/components/add-to-cart-button.js
@@ -5,50 +5,50 @@ import { _x } from '@wordpress/i18n';
 
 export default function AddToCartButton( { container, passesValidation } )
 {
-    if (! container.id ) {
-        return null;
-    }
+	if (! container.id ) {
+		return null;
+	}
 
-    const inStock = !! container.is_in_stock;
-    const isPurchasable = !! container.is_purchasable;
+	const inStock = !! container.is_in_stock;
+	const isPurchasable = !! container.is_purchasable;
 
-    const handleAddToCart = ( event ) => {
-        const productId = event.currentTarget.getAttribute('data-form_id');
+	const handleAddToCart = ( event ) => {
+		const productId = event.currentTarget.getAttribute('data-form_id');
 
-        const form = document.querySelector(
-            `form[data-product_id="${ productId }"]`
-        );
+		const form = document.querySelector(
+			`form[data-product_id="${ productId }"]`
+		);
 
-        if ( form ) {
-            const submitButton = form.querySelector('[type="submit"]');
+		if ( form ) {
+			const submitButton = form.querySelector('[type="submit"]');
 
-            // Need to click on submit button to ensure that the 'add-to-cart' value is sent for simple MNM.
-            if ( submitButton && ! event.currentTarget.classList.contains('disabled') ) {
-                submitButton.click();
-            }
-        }
+			// Need to click on submit button to ensure that the 'add-to-cart' value is sent for simple MNM.
+			if ( submitButton && ! event.currentTarget.classList.contains('disabled') ) {
+				submitButton.click();
+			}
+		}
 
-    };
+	};
 
-    if (! inStock || ! isPurchasable ) {
-        return null;
-    }
+	if (! inStock || ! isPurchasable ) {
+		return null;
+	}
 
-    // By default, the store API gets the loop's add to cart text, which we modify to be select options so we need a different text.
-    const addToCartText = container?.extensions?.mix_and_match.single_add_to_cart_text ??  _x('Add to cart', '[Frontend]', 'wc-mnm-mobile-styles');
+	// By default, the store API gets the loop's add to cart text, which we modify to be select options so we need a different text.
+	const addToCartText = container?.extensions?.mix_and_match.single_add_to_cart_text ??  _x('Add to cart', '[Frontend]', 'wc-mnm-mobile-styles');
 
-    return (
-        <button
-            type="button"
-            data-form_id={
-                container.parent > 0 ? container.parent : container.id
-            }
-            onClick={ handleAddToCart }
-            className={ `single_add_to_cart_button button alt wp-element-button ${
-                ! passesValidation ? 'disabled' : ''
-            }` }
-        >
-        { addToCartText }
-        </button>
-    );
+	return (
+		<button
+			type="button"
+			data-form_id={
+				container.parent > 0 ? container.parent : container.id
+			}
+			onClick={ handleAddToCart }
+			className={ `single_add_to_cart_button button alt wp-element-button ${
+				! passesValidation ? 'disabled' : ''
+			}` }
+		>
+		{ addToCartText }
+		</button>
+	);
 }
--- src/components/cancel-button.js
+++ src/components/cancel-button.js
@@ -5,29 +5,29 @@ import { _x } from '@wordpress/i18n';
 
 export default function CancelButton( { context } )
 {
-    if ('edit' !== context ) {
-        return null;
-    }
+	if ('edit' !== context ) {
+		return null;
+	}
 
-    const onCancelEdit = () => {
-        console.debug('cancel edit');
-    };
+	const onCancelEdit = () => {
+		console.debug('cancel edit');
+	};
 
-    return (
-    <div className="wc-mnm-edit-subscription-actions woocommerce-cart-form">
-    <div className="actions">
-                <button
-                    type="button"
-                    className="button wc-mnm-cancel-edit wp-element-button"
-                    onClick={ onCancelEdit }
-                >
-                    { _x(
-                        'Cancel edit',
-                        '[Frontend]',
-                        'wc-mnm-mobile-styles'
-                    ) }
-                </button>
-    </div>
-    </div>
-    );
+	return (
+	<div className="wc-mnm-edit-subscription-actions woocommerce-cart-form">
+	<div className="actions">
+				<button
+					type="button"
+					className="button wc-mnm-cancel-edit wp-element-button"
+					onClick={ onCancelEdit }
+				>
+					{ _x(
+						'Cancel edit',
+						'[Frontend]',
+						'wc-mnm-mobile-styles'
+					) }
+				</button>
+	</div>
+	</div>
+	);
 }
